16e095bbb947fca5d385356b0d752c8dfb85bbf8
onurduyar <onurdyr510@gmail.com>
2022-10-19 14:11:14 +0300

    project was initialized

---
+++ Flickr App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flickr App
+//
+//  Created by Onur Duyar on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
